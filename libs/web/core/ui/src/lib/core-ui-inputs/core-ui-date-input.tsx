3acedfda65bc3015b0e0cc1e986b534452949135
panosdb <ppsimatikas@gmail.com>
2024-06-10 18:26:13 +0300

    Move the set value to demo page

--- libs/web/core/ui/src/lib/core-ui-inputs/core-ui-date-input.tsx
+++ libs/web/core/ui/src/lib/core-ui-inputs/core-ui-date-input.tsx
@@ -3,14 +3,11 @@ import { DateInput, DateInputProps } from '@mantine/dates'
 import { IconCalendarMonth } from '@tabler/icons-react'
 
 export function CoreUiDateInput({ ...props }: DateInputProps) {
-  const [value, setValue] = useState<Date | null>(null);
   const inputRef = useRef<HTMLInputElement>(null);
 
   return (
     <DateInput
       ref={inputRef}
-      value={props.value || value}
-      onChange={props.onChange || setValue}
       rightSection={<IconCalendarMonth onClick={() => inputRef.current?.focus() } style={{ cursor: 'pointer' }}/>}
       {...props}
     />
