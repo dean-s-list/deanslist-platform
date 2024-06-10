d30821ca82ce317f8506b990bc980b26b707432f
panosdb <ppsimatikas@gmail.com>
2024-06-10 17:08:44 +0300

    Fix linting

--- libs/api/rating/data-access/src/lib/helpers/get-admin-rating-where.input.ts
+++ libs/api/rating/data-access/src/lib/helpers/get-admin-rating-where.input.ts
@@ -1,9 +1,7 @@
 import { Prisma } from '@prisma/client'
 import { AdminFindManyRatingInput } from '../dto/admin-find-many-rating.input'
 
-export function getAdminRatingWhereInput(
-  input: AdminFindManyRatingInput,
-): Prisma.RatingWhereInput {
+export function getAdminRatingWhereInput(input: AdminFindManyRatingInput): Prisma.RatingWhereInput {
   const where: Prisma.RatingWhereInput = {}
 
   if (input.search) {
--- libs/web/rating/feature/src/index.ts
+++ libs/web/rating/feature/src/index.ts
@@ -1,4 +1,4 @@
-export const UserRatingFeature = lazy(() => import('./lib/user-rating.routes'))
 import { lazy } from 'react'
+export const UserRatingFeature = lazy(() => import('./lib/user-rating.routes'))
 
 export const AdminRatingFeature = lazy(() => import('./lib/admin-rating.routes'))
