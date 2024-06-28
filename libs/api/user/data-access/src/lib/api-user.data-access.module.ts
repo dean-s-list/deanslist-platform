a1e17f34fe7f59b75c1fa35cd5ecaa02ccea98c1
Bram Borggreve <beeman@beeman.nl>
2024-06-28 22:29:47 +0100

    feat: add user-identity map endpoint

--- libs/api/user/data-access/src/lib/api-user.data-access.module.ts
+++ libs/api/user/data-access/src/lib/api-user.data-access.module.ts
@@ -1,14 +1,22 @@
 import { ApiCoreDataAccessModule } from '@deanslist-platform/api-core-data-access'
 import { Module } from '@nestjs/common'
+import { ApiUserDataAdminService } from './api-user-data-admin.service'
+import { ApiUserDataAnonService } from './api-user-data-anon.service'
+import { ApiUserDataUserService } from './api-user-data-user.service'
 import { ApiUserDataService } from './api-user-data.service'
 import { ApiUserEventService } from './api-user-event.service'
-import { ApiUserDataAdminService } from './api-user-data-admin.service'
-import { ApiUserDataUserService } from './api-user-data-user.service'
 import { ApiUserService } from './api-user.service'
 
 @Module({
   imports: [ApiCoreDataAccessModule],
-  providers: [ApiUserService, ApiUserDataService, ApiUserEventService, ApiUserDataAdminService, ApiUserDataUserService],
+  providers: [
+    ApiUserService,
+    ApiUserDataService,
+    ApiUserDataAnonService,
+    ApiUserEventService,
+    ApiUserDataAdminService,
+    ApiUserDataUserService,
+  ],
   exports: [ApiUserService],
 })
 export class ApiUserDataAccessModule {}
