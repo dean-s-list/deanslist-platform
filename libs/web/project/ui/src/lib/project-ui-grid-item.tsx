8458c40a5791d6bba379e6d1f6fbcc576a5976a3
Bram Borggreve <beeman@beeman.nl>
2024-07-10 16:16:22 +0200

    fix: make project grid item clickable

--- libs/web/project/ui/src/lib/project-ui-grid-item.tsx
+++ libs/web/project/ui/src/lib/project-ui-grid-item.tsx
@@ -1,19 +1,18 @@
 import type { Project } from '@deanslist-platform/sdk'
-import { CoreUiDebugModal } from '@deanslist-platform/web-core-ui'
 import { Paper, Text } from '@mantine/core'
 import { UiGroup, UiStack } from '@pubkey-ui/core'
+import { Link } from 'react-router-dom'
 import { ProjectUiAmount } from './project-ui-amount'
 import { ProjectUiItem } from './project-ui-item'
 import { ProjectUiStatusBadge } from './project-ui-status-badge'
 import { ProjectUiTags } from './project-ui-tags'
 
-export function ProjectUiGridItem({ project, to }: { project: Project; to?: string }) {
+export function ProjectUiGridItem({ project, to }: { project: Project; to: string }) {
   return (
-    <Paper withBorder p="md" radius="lg">
+    <Paper component={Link} to={to} withBorder p="md" radius="lg">
       <UiStack>
         <UiGroup>
-          <ProjectUiItem project={project} to={to} />
-          <CoreUiDebugModal data={project} />
+          <ProjectUiItem project={project} />
         </UiGroup>
         <ProjectUiTags tags={project.tags} />
         <ProjectUiAmount amount={project.amountTotalUsd} label="total" />
