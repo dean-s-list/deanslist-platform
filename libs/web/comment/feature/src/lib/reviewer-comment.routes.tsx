d29d8615c32be09a76b47e5cdccecedb702bfd98
panosdb <ppsimatikas@gmail.com>
2024-07-16 17:07:28 +0300

    Code format

--- libs/web/comment/feature/src/lib/reviewer-comment.routes.tsx
+++ libs/web/comment/feature/src/lib/reviewer-comment.routes.tsx
@@ -4,10 +4,5 @@ import { useRoutes } from 'react-router-dom'
 import { ReviewerCommentListFeature } from './reviewer-comment-list.feature'
 
 export default function ReviewerCommentRoutes({ review, leftAction }: { review: Review; leftAction: ReactNode }) {
-  return useRoutes([
-    {
-      path: '',
-      element: <ReviewerCommentListFeature review={review} leftAction={leftAction} />,
-    },
-  ])
+  return useRoutes([{ path: '', element: <ReviewerCommentListFeature review={review} leftAction={leftAction} /> }])
 }
