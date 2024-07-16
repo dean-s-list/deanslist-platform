443e4ef25c69e70c106f4c1dc4093b7bd4131c86
panosdb <ppsimatikas@gmail.com>
2024-07-16 16:43:17 +0300

    Add real deadline in days

--- libs/web/project/ui/src/lib/project-ui-deadline.tsx
+++ libs/web/project/ui/src/lib/project-ui-deadline.tsx
@@ -2,12 +2,25 @@ import { Project } from '@deanslist-platform/sdk'
 import { Group, Text } from '@mantine/core'
 import { IconCalendar } from '@tabler/icons-react'
 
+function getRemainingDays(date: Date): number {
+  const now = new Date().getTime()
+  const target = date.getTime()
+  const difference = target - now
+
+  if (difference < 0) {
+    return 0
+  }
+
+  return Math.floor(difference / (1000 * 60 * 60 * 24))
+}
+
 export function ProjectUiDeadline({ project }: { project: Project }) {
-  // TODO: Add deadline on project object
+  const days = project.endDate ? getRemainingDays(project.endDate) : 0
+
   return (
     <Group gap={5}>
       <IconCalendar size={16} />
-      <Text>10 days left</Text>
+      <Text>{days} days left</Text>
     </Group>
   )
 }
--- libs/web/project/ui/src/lib/project-ui-grid-item.tsx
+++ libs/web/project/ui/src/lib/project-ui-grid-item.tsx
@@ -1,6 +1,6 @@
 import type { Project } from '@deanslist-platform/sdk'
 import { CoreUiCard } from '@deanslist-platform/web-core-ui'
-import { Divider } from '@mantine/core'
+import { Divider, Group } from '@mantine/core'
 import { UiGroup, UiStack } from '@pubkey-ui/core'
 import { ProjectUiAmount } from './project-ui-amount'
 import { ProjectUiItem } from './project-ui-item'
@@ -8,20 +8,24 @@ import { ProjectUiStatusBadge } from './project-ui-status-badge'
 import { ProjectUiTags } from './project-ui-tags'
 import { IconExternalLink } from '@tabler/icons-react'
 import { ProjectUiParticipants } from './project-ui-participants'
+import { ProjectUiDeadline } from './project-ui-deadline'
 
 export function ProjectUiGridItem({ project, to }: { project: Project; to: string }) {
   return (
     <CoreUiCard to={to}>
       <UiStack>
         <UiGroup align="start">
-          <ProjectUiItem project={project} />
+          <Group gap="md">
+            <ProjectUiItem project={project} />
+            <ProjectUiStatusBadge status={project?.status} />
+          </Group>
           <IconExternalLink />
         </UiGroup>
         <ProjectUiTags tags={project.tags} />
         <ProjectUiAmount amount={project.amountTotalUsd} label="total" />
         <Divider />
         <UiGroup>
-          <ProjectUiStatusBadge status={project?.status} />
+          <ProjectUiDeadline project={project} />
           <ProjectUiParticipants project={project} />
         </UiGroup>
       </UiStack>
