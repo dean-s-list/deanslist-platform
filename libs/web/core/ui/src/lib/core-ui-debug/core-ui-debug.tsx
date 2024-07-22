537523300c582863608b8feea4707667b62db92c
Bram Borggreve <beeman@beeman.nl>
2024-07-22 22:23:16 +0200

    fix: make core-ui-debug component extend Paper component

--- libs/web/core/ui/src/lib/core-ui-debug/core-ui-debug.tsx
+++ libs/web/core/ui/src/lib/core-ui-debug/core-ui-debug.tsx
@@ -1,4 +1,4 @@
-import { Box, Group, Paper, Text, UnstyledButton, useMantineTheme } from '@mantine/core'
+import { Box, Group, Paper, PaperProps, Text, UnstyledButton, useMantineTheme } from '@mantine/core'
 import { IconEye, IconEyeOff } from '@tabler/icons-react'
 import { ReactNode, useState } from 'react'
 
@@ -6,7 +6,8 @@ export function CoreUiDebug({
   data,
   hideButton,
   open,
-}: {
+  ...props
+}: PaperProps & {
   data: string | unknown
   open?: boolean
   hideButton?: boolean
@@ -15,25 +16,24 @@ export function CoreUiDebug({
   const [show, setShow] = useState(open)
   const content: ReactNode = typeof data === 'string' ? data : JSON.stringify(data, null, 2)
   return (
-    <Box>
-      <Paper
-        component="pre"
-        fz="xs"
-        m={0}
-        p={theme.spacing.xs}
-        style={{ overflow: 'auto', textOverflow: 'ellipsis' }}
-        withBorder
-      >
-        {hideButton ? null : (
-          <UnstyledButton onClick={() => setShow(!show)}>
-            <Group p="xs" gap="xs">
-              {show ? <IconEyeOff size={16} /> : <IconEye size={16} />}
-              <Text size="xs">{show ? 'Hide' : 'Show'} debug data</Text>
-            </Group>
-          </UnstyledButton>
-        )}
-        <Box display={show ? 'block' : 'none'}>{content}</Box>
-      </Paper>
-    </Box>
+    <Paper
+      component="pre"
+      fz="xs"
+      m={0}
+      p={theme.spacing.xs}
+      style={{ overflow: 'auto', textOverflow: 'ellipsis' }}
+      withBorder
+      {...props}
+    >
+      {hideButton ? null : (
+        <UnstyledButton onClick={() => setShow(!show)}>
+          <Group p="xs" gap="xs">
+            {show ? <IconEyeOff size={16} /> : <IconEye size={16} />}
+            <Text size="xs">{show ? 'Hide' : 'Show'} debug data</Text>
+          </Group>
+        </UnstyledButton>
+      )}
+      <Box display={show ? 'block' : 'none'}>{content}</Box>
+    </Paper>
   )
 }
